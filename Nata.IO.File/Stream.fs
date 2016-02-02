f2f94b8a3cd64c14eeda19c762a5dbfea57dc064
Jonathan Leaver <jonathan@jet.com>
2016-02-02 18:23:45 -0500

    further refine file i/o model

--- Nata.IO.File/Stream.fs
+++ Nata.IO.File/Stream.fs
@@ -11,61 +11,54 @@ module Stream =
     type Index = int
     type Path = string
 
+    type private Message = AsyncReplyChannel<unit> * string option
     
     let create (path:Path) =
     
-        let stream =
+        let openStream() =
             let mode = FileMode.OpenOrCreate
             let access = FileAccess.ReadWrite
             let share = FileShare.ReadWrite
             new FileStream(path,mode,access,share)
-        let writer = new StreamWriter(stream)
+
+        let writer = new StreamWriter(openStream())
 
         let count() =
-            use stream =
-                let mode = FileMode.Open
-                let access = FileAccess.ReadWrite
-                let share = FileShare.ReadWrite
-                new FileStream(path,mode,access,share)
-            use reader = new StreamReader(stream)
-                
-            let i = ref 0
-            while (not reader.EndOfStream) do
-                i := 1 + !i
-            !i
+            seq {
+                use reader = new StreamReader(openStream())
+                while (not reader.EndOfStream) do
+                    yield reader.ReadLine()
+            } |> Seq.length
 
         let lines = ref (count())
 
-        let actor = MailboxProcessor<string option>.Start <| fun inbox ->
+        let actor = MailboxProcessor<Message>.Start <| fun inbox ->
             let rec loop() =
                 async {
-                    let! input = inbox.Receive()
+                    let! sender, input = inbox.Receive()
                     match input with
                     | None ->
                         writer.Close()
-                        stream.Close()
+                        sender.Reply()
                         return ()
                     | Some line ->
                         writer.WriteLine(line)
                         writer.Flush()
                         Interlocked.Increment(&lines.contents) |> ignore
+                        sender.Reply()
                         return! loop()
                 }
             loop()
 
         let write(line)=
-            Some line |> actor.Post
+            actor.PostAndReply(fun sender -> sender, Some line)
 
         let close() =
-            None |> actor.Post
+            actor.PostAndReply(fun sender -> sender, None)
 
         let read() =
             seq {
-                use stream =
-                    let mode = FileMode.Open
-                    let access = FileAccess.ReadWrite
-                    let share = FileShare.ReadWrite
-                    new FileStream(path,mode,access,share)
+                use stream = openStream()
                 use reader = new StreamReader(stream)
                 
                 let i = ref 0
