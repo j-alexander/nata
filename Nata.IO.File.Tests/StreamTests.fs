69af08b564fb077b9fc8d536f3d2b2c3e2feb195
Jonathan Leaver <jonathan@jet.com>
2016-02-02 17:28:18 -0500

    add multiple-reader + single-writer file io and preliminary test

---
+++ Nata.IO.File.Tests/StreamTests.fs
@@ -0,0 +1,47 @@
+﻿namespace Nata.IO.File.Tests
+
+open System
+open System.Collections.Concurrent
+open System.Threading
+open System.Threading.Tasks
+
+open NUnit.Framework
+open Nata.IO
+
+[<TestFixture>]
+type StreamTests() =
+
+    [<Test>]
+    member x.TestFiles2() =
+
+        let path = Guid.NewGuid().ToString("n") |> sprintf @"c:\users\jonathan\desktop\%s.txt" 
+        let read,write,close = File.Stream.create path
+        
+        let messages = new BlockingCollection<string>()
+        
+        let format = sprintf "written line %d completed" 
+
+        Task.Run(fun _ ->
+            [1..1000000]
+            |> Seq.iter (format >> write)
+            messages.Add("Writer done!")) |> ignore
+        [1..75]
+        |> Seq.iter (fun worker ->
+            Thread.Sleep(10)
+            Task.Run(fun _ ->
+                let count = ref 0
+                for i, line in read() |> Seq.mapi(fun i x -> i+1,x) do
+                    count := 1 + !count
+                    if format i <> line then
+                        messages.Add(sprintf "Worker %d finds #%d '%s' <> '%s'" worker i (format i) line)
+
+
+                !count
+                |> sprintf "Worker %d read %d" worker
+                |> messages.Add)
+            |> ignore)
+
+        for i in [ 1 .. 76 ] do
+            messages.Take() |> printfn "%s"
+
+        close()
---
+++ Nata.IO.File/Stream.fs
@@ -0,0 +1,76 @@
+﻿namespace Nata.IO.File
+
+open System
+open System.IO
+open System.Threading
+open Nata.IO
+
+module Stream =
+
+    type Settings = unit
+    type Index = int
+    type Path = string
+
+    
+    let create (path:Path) =
+    
+        let stream =
+            let mode = FileMode.OpenOrCreate
+            let access = FileAccess.ReadWrite
+            let share = FileShare.ReadWrite
+            new FileStream(path,mode,access,share)
+        let writer = new StreamWriter(stream)
+
+        let count() =
+            use stream =
+                let mode = FileMode.Open
+                let access = FileAccess.ReadWrite
+                let share = FileShare.ReadWrite
+                new FileStream(path,mode,access,share)
+            use reader = new StreamReader(stream)
+                
+            let i = ref 0
+            while (not reader.EndOfStream) do
+                i := 1 + !i
+            !i
+
+        let lines = ref (count())
+
+        let actor = MailboxProcessor<string option>.Start <| fun inbox ->
+            let rec loop() =
+                async {
+                    let! input = inbox.Receive()
+                    match input with
+                    | None ->
+                        writer.Close()
+                        stream.Close()
+                        return ()
+                    | Some line ->
+                        writer.WriteLine(line)
+                        writer.Flush()
+                        Interlocked.Increment(&lines.contents) |> ignore
+                        return! loop()
+                }
+            loop()
+
+        let write(line)=
+            Some line |> actor.Post
+
+        let close() =
+            None |> actor.Post
+
+        let read() =
+            seq {
+                use stream =
+                    let mode = FileMode.Open
+                    let access = FileAccess.ReadWrite
+                    let share = FileShare.ReadWrite
+                    new FileStream(path,mode,access,share)
+                use reader = new StreamReader(stream)
+                
+                let i = ref 0
+                while (!i < !lines && not reader.EndOfStream) do
+                    yield reader.ReadLine()
+            }
+
+        read,write,close
