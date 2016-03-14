6dd9040e18100b5ea0a1e98f2dbf8d413a929d8c
Jonathan Leaver <jonathan@jet.com>
2016-03-14 17:01:32 -0400

    include kafka-net library

--- Nata.IO.Kafka/Partition.fs
+++ Nata.IO.Kafka/Partition.fs
@@ -1,5 +1,13 @@
 ﻿namespace Nata.IO.Kafka
 
+open System
+open System.Net
+open System.Text
+open NLog.FSharp
+open KafkaNet
+open KafkaNet.Model
+open KafkaNet.Protocol
+
 module Partition =
     
     let x = 3
