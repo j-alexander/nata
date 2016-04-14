a71ddcaf15b97bbf384d05c7af1280aad0e4aec1
Jonathan Leaver <jleaver@alumni.uwo.ca>
2016-04-13 22:24:45 -0400

    simplify handling of un-initialized blobs

--- Nata.IO.AzureStorage/Blob.fs
+++ Nata.IO.AzureStorage/Blob.fs
@@ -2,7 +2,6 @@
 
 open System
 open System.IO
-open System.Net
 open Microsoft.WindowsAzure.Storage
 open Microsoft.WindowsAzure.Storage.Blob
 open Nata.IO
@@ -43,34 +42,31 @@ module Blob =
                 | Position.At etag -> AccessCondition.GenerateIfMatchCondition(etag)
                 | Position.After x -> AccessCondition.GenerateEmptyCondition()
                 | Position.End -> AccessCondition.GenerateEmptyCondition()
-            let generator =
-                Seq.unfold <| fun (position:Position<string>) ->
-                    try 
-                        use memory = new MemoryStream()
-                        reference.DownloadToStream(memory, condition position)
+            Seq.unfold <| fun (position:Position<string>) ->
+                try 
+                    use memory = new MemoryStream()
+                    reference.DownloadToStream(memory, condition position)
                     
-                        let bytes = memory.ToArray()
-                        let withLastModified =
-                            match reference.Properties.LastModified with
-                            | x when x.HasValue -> Event.withCreatedAt x.Value.UtcDateTime
-                            | x -> id
-                        let etag, contentType =
-                            reference.Properties.ETag,
-                            reference.Properties.ContentType
-                        let event =
-                            Event.create bytes
-                            |> Event.withName blobName
-                            |> Event.withTag etag
-                            |> Event.withEventType contentType
-                            |> withLastModified
-                        Some ((event, etag), position)
-                    with
-                    | :? StorageException as e when e.RequestInformation.HttpStatusCode = 412 ->
-                        None
-            fun position ->
-                seq {
-                    if reference.Exists() then yield! generator position
-                }
+                    let bytes = memory.ToArray()
+                    let withLastModified =
+                        match reference.Properties.LastModified with
+                        | x when x.HasValue -> Event.withCreatedAt x.Value.UtcDateTime
+                        | x -> id
+                    let etag, contentType =
+                        reference.Properties.ETag,
+                        reference.Properties.ContentType
+                    let event =
+                        Event.create bytes
+                        |> Event.withName blobName
+                        |> Event.withTag etag
+                        |> Event.withEventType contentType
+                        |> withLastModified
+                    Some ((event, etag), position)
+                with
+                | :? StorageException as e when
+                    [ 404   // 404 - blob does not yet exist
+                      412 ] // 412 - etag for blob has expired or is invalid
+                    |> List.exists ((=) e.RequestInformation.HttpStatusCode) -> None
 
         let read container blobName =
             readFrom container blobName Position.End |> Seq.map fst
