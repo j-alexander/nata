468b950aeae9c31fbfd9287300f39076af7cf06e
Jonathan Leaver <jonathan@jet.com>
2016-07-15 17:38:43 -0400

    add a test to demonstrate websockets in action

--- Nata.IO.WebSocket.Tests/SocketTests.fs
+++ Nata.IO.WebSocket.Tests/SocketTests.fs
@@ -1,9 +1,27 @@
 ﻿namespace Nata.IO.WebSocket.Tests
 
+open System
+open Nata.IO
+open Nata.IO.Capability
+open Nata.IO.WebSocket
+open Nata.IO.WebSocket.Socket
 open NUnit.Framework
 
 [<TestFixture>]
 type SocketTests() = 
 
-    [<SetUp>]
-    member x.SetUp() = ()
+    let settings = { Host="ws://ws.websocketstest.com:80/service"; AutoPingInterval=None }
+
+    [<Test; Timeout(10000)>]
+    member x.TestTimeService() =
+        let write, subscribe =
+            let socket = Socket.connect settings
+            writer socket,
+            subscriber socket
+        write (Event.create "timer,")
+        let time =
+            subscribe()
+            |> Seq.take 3
+            |> Seq.last
+            |> Event.data
+        Assert.True(time.Contains(DateTime.UtcNow.Year.ToString()))
