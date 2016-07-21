3880978502309a534a44d70dc7403159cd440b55
Jonathan Leaver <jonathan@jet.com>
2016-07-21 14:45:33 -0400

    create a project for writing development scripts across data sources

---
+++ Nata.Script/Script.fsx
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+
+#r @"C:\Users\Jonathan\Source\nata\packages\FSharp.Data\lib\net40\FSharp.Data.dll"
+open FSharp.Data
+
+#r @"bin/Debug/NLog.dll"
+#r @"bin/Debug/NLog.FSharp.dll"
+#r @"bin/Debug/Newtonsoft.Json.dll"
+#r @"bin/Debug/EventStore.ClientAPI.dll"
+#r @"bin/Debug/kafka-net.dll"
+#r @"bin/Debug/Nata.IO.dll"
+#r @"bin/Debug/Nata.IO.EventStore.dll"
+#r @"bin/Debug/Nata.IO.Kafka.dll"
+
+open Nata
+open Nata.IO
+open Nata.IO.EventStore
+open Nata.IO.Kafka
+open Nata.IO.Capability
+
