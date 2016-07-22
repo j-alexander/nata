b04518daa36914fefa3803e7be8fd068ac9a7b4e
Jonathan Leaver <jonathan@jet.com>
2016-07-22 15:54:22 -0400

    use FSharp.Data from output folder, include RabbitMQ in Script.fsx

--- Nata.Script/Script.fsx
+++ Nata.Script/Script.fsx
@@ -1,22 +1,27 @@
 ﻿open System
 open System.IO
 
-#r @"C:\Users\Jonathan\Source\nata\packages\FSharp.Data\lib\net40\FSharp.Data.dll"
+#r @"bin/Debug/FSharp.Data.dll"
 open FSharp.Data
 
 #r @"bin/Debug/NLog.dll"
 #r @"bin/Debug/NLog.FSharp.dll"
 #r @"bin/Debug/Newtonsoft.Json.dll"
+
 #r @"bin/Debug/EventStore.ClientAPI.dll"
 #r @"bin/Debug/kafka-net.dll"
+#r @"bin/Debug/RabbitMQ.Client.dll"
+
 #r @"bin/Debug/Nata.Fun.JsonPath.dll"
 #r @"bin/Debug/Nata.IO.dll"
 #r @"bin/Debug/Nata.IO.EventStore.dll"
 #r @"bin/Debug/Nata.IO.Kafka.dll"
+#r @"bin/Debug/Nata.IO.RabbitMQ.dll"
 
 open Nata
+open Nata.Fun.JsonPath
 open Nata.IO
+open Nata.IO.Capability
 open Nata.IO.EventStore
 open Nata.IO.Kafka
-open Nata.IO.Capability
-open Nata.Fun.JsonPath
+open Nata.IO.RabbitMQ
