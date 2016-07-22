7265f421e7c1315d219d860fa446f6cb14c319f5
Jonathan Leaver <jonathan@jet.com>
2016-07-22 15:57:56 -0400

    add file and memory to Script.fsx

--- Nata.Script/Script.fsx
+++ Nata.Script/Script.fsx
@@ -1,12 +1,11 @@
 ﻿open System
 open System.IO
 
-#r @"bin/Debug/FSharp.Data.dll"
-open FSharp.Data
-
 #r @"bin/Debug/NLog.dll"
 #r @"bin/Debug/NLog.FSharp.dll"
 #r @"bin/Debug/Newtonsoft.Json.dll"
+#r @"bin/Debug/FSharp.Data.dll"
+open FSharp.Data
 
 #r @"bin/Debug/EventStore.ClientAPI.dll"
 #r @"bin/Debug/kafka-net.dll"
@@ -14,6 +13,8 @@ open FSharp.Data
 
 #r @"bin/Debug/Nata.Fun.JsonPath.dll"
 #r @"bin/Debug/Nata.IO.dll"
+#r @"bin/Debug/Nata.IO.Memory.dll"
+#r @"bin/Debug/Nata.IO.File.dll"
 #r @"bin/Debug/Nata.IO.EventStore.dll"
 #r @"bin/Debug/Nata.IO.Kafka.dll"
 #r @"bin/Debug/Nata.IO.RabbitMQ.dll"
@@ -22,6 +23,8 @@ open Nata
 open Nata.Fun.JsonPath
 open Nata.IO
 open Nata.IO.Capability
+open Nata.IO.Memory
+open Nata.IO.File
 open Nata.IO.EventStore
 open Nata.IO.Kafka
 open Nata.IO.RabbitMQ
