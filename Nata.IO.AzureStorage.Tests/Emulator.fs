f0ad28ca2f35d0d72c1e8d00985f7d0f10605518
Jonathan Leaver <jonathan@jet.com>
2016-11-01 13:27:08 -0400

    expose azure storage emulator via docker

--- Nata.IO.AzureStorage.Tests/Emulator.fs
+++ Nata.IO.AzureStorage.Tests/Emulator.fs
@@ -8,3 +8,15 @@ module Emulator =
         let key = @"Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
         let connectionString = "UseDevelopmentStorage=true"
 
+
+
+        let dockerConnectionString ip =
+            "DefaultEndpointsProtocol=http;" + 
+            "BlobEndpoint=http://"+ip+":10000/dockerstorage;" +
+            "QueueEndpoint=http://"+ip+":10001/dockerstorage;" +
+            "TableEndpoint=http://"+ip+":10002/dockerstorage;" +
+            "AccountName=dockerstorage;" +
+            "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
+
+        let exampleDockerConnectionString =
+            dockerConnectionString "192.168.137.226"
