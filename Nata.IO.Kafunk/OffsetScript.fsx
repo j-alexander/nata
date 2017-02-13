06435627ef5b4ed66ea062cf8198fde2ce21a6a8
Jonathan Leaver <jonathan@jet.com>
2017-02-13 18:15:42 -0500

    simplify offset range calculation

--- Nata.IO.Kafunk/OffsetScript.fsx
+++ Nata.IO.Kafunk/OffsetScript.fsx
@@ -8,34 +8,18 @@ open Kafunk
 let watermarksFor (connection:KafkaConn) (topic:string) =
     async {
         let! metadata = Kafka.metadata connection (Metadata.Request [| topic |])
-        match metadata.topicMetadata
-              |> Array.tryFind (fun x -> x.topicName = topic) with
+        let topicMetadata = 
+            metadata.topicMetadata
+            |> Array.tryFind (fun tm -> tm.topicName = topic)
+        match topicMetadata with
         | None ->
-            return [||]
-        | Some topicMetadata ->
-            let request (time:Time) (fn) =
-                new OffsetRequest(-1,
-                    [| topicMetadata.topicName,
-                       [| for p in topicMetadata.partitionMetadata ->
-                            p.partitionId, time, MaxNumberOfOffsets.MaxValue |] |])
-                |> Kafka.offset connection
-                |> Async.map (fun response ->
-                    response.topics
-                    |> Array.filter (fst >> (=) topic)
-                    |> Array.collect (snd >> Array.map (fun po ->
-                        po.partition,
-                        fn po.offsets))
-                    |> Array.sortBy fst)
-            let! lows = request Time.EarliestOffset Array.min
-            let! highs = request Time.LatestOffset Array.max
-            return
-                Seq.toArray <|
-                    query {
-                        for (lp,lo) in lows do
-                        join (hp,ho) in highs on (lp = hp)
-                        select (topic,lp,lo,ho)
-                    }
-
+            return []
+        | Some tm ->
+            return!
+                tm.partitionMetadata
+                |> Seq.map (fun pm -> pm.partitionId)
+                |> Offsets.offsetRange connection topic
+                |> Async.map (Map.toList)
     }
 
 let host = "tcp://guardians-kafka-cluster.qa.jet.com:9092"
