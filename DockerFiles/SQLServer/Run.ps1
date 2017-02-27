92da9dd958af34fc699659663303ffb79e2b666e
Jonathan Leaver <jonathan@jet.com>
2017-02-27 13:32:19 -0500

    generate sqlserver image using default powershell

--- DockerFiles/SQLServer/Run.ps1
+++ DockerFiles/SQLServer/Run.ps1
@@ -1,3 +1,3 @@
 Write-Host "Use a connection string as follows, after setting {ip}:"
-Write-Host "Server={ip};User Id=sa;Password=docker_13.0.1601.5"
-docker run -it -p 1433:1433 sqlserver
+Write-Host "Server={ip};User Id=sa;Password=docker_13.0.4001.0"
+docker run --rm -p 1433:1433 -it sqlserver
