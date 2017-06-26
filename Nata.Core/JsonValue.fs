6cf3598692a9bd3d60d6b099c443baa470ae5e83
Jonathan Leaver <jleaver@alumni.uwo.ca>
2017-06-26 15:20:31 -0400

    add json converters for common fsharp types

--- Nata.Core/JsonValue.fs
+++ Nata.Core/JsonValue.fs
@@ -1,11 +1,125 @@
 ﻿namespace Nata.Core
 
+open System
 open System.Text
 open FSharp.Data
 open Newtonsoft.Json
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module JsonValue =
+
+    module Converters =
+    
+        open FSharp.Reflection
+
+        type public OptionConverter() =
+            inherit JsonConverter()
+
+            override x.CanConvert(t) = 
+                t.IsGenericType &&
+                t.GetGenericTypeDefinition() = typedefof<option<_>>
+
+            override x.WriteJson(writer, value, serializer) =
+                let value = 
+                    if value = null then null
+                    else 
+                        let _,fields = FSharpValue.GetUnionFields(value, value.GetType())
+                        fields.[0]  
+                serializer.Serialize(writer, value)
+
+            override x.ReadJson(reader, t, existingValue, serializer) =        
+                let innerType = t.GetGenericArguments().[0]   
+                match innerType.IsValueType, reader.Value with
+                | true, null ->
+                    None :> obj
+                | _ -> 
+                    try let value = serializer.Deserialize(reader, innerType)
+                        let cases = FSharpType.GetUnionCases(t)
+                        if value = null then FSharpValue.MakeUnion(cases.[0], [||])
+                        else FSharpValue.MakeUnion(cases.[1], [|value|])
+                    with _ -> None :> obj
+
+
+        type TupleConverter() =
+            inherit JsonConverter()
+    
+            override x.CanConvert(t:Type) = 
+                FSharpType.IsTuple(t)
+
+            override x.WriteJson(writer, value, serializer) =
+                serializer.Serialize(writer, FSharpValue.GetTupleFields(value))
+
+            override x.ReadJson(reader, t, _, serializer) =
+                let elements =
+                    [|
+                        reader.Read() |> ignore
+                        for element in FSharpType.GetTupleElements(t) do
+                            yield serializer.Deserialize(reader, element)
+                            reader.Read() |> ignore
+                    |]
+               
+                FSharpValue.MakeTuple(elements, t)
+        
+
+        type ValueAttribute(value : obj, [<ParamArray>]additional : obj array) =
+            inherit Attribute()
+
+            new(value : obj) = ValueAttribute(value, [||])
+
+            member x.Values = [ yield value
+                                yield! additional ]
+
+
+        type public UnionConverter() =
+            inherit JsonConverter()
+
+            override x.CanConvert(t : Type) =
+                FSharpType.IsUnion(t) &&
+                FSharpType.GetUnionCases(t) |> Seq.forall (fun z -> z.GetFields().Length = 0)
+
+            override x.WriteJson(writer : JsonWriter, value, serializer) =
+                let case, fields = FSharpValue.GetUnionFields(value, value.GetType())
+                let values =
+                    case.GetCustomAttributes()
+                    |> Seq.choose(function :? ValueAttribute as x -> Some x | _ -> None)
+                    |> Seq.toList
+                match values with
+                | x :: _ -> match x.Values.Head with
+                            | :? int as value -> writer.WriteValue(value)
+                            | :? string as value -> writer.WriteValue(value)
+                            | _ -> writer.WriteValue(case.Name)
+                | [] -> writer.WriteValue(case.Name)
+
+            override x.ReadJson(reader : JsonReader, objectType, existingValue, serializer) =
+                let value = reader.Value.ToString()
+                FSharpType.GetUnionCases(objectType)
+                |> List.ofSeq
+                |> List.filter(fun case ->
+                    let values =
+                        case.GetCustomAttributes()
+                        |> Seq.choose(function :? ValueAttribute as x -> Some x | _ -> None)
+                        |> Seq.toList
+                    match values with
+                    | x :: _ ->
+
+                        x.Values
+                        |> Seq.exists(function | :? string as x -> 0 = String.Compare(x, value, true)
+                                               | :? int as x -> match value |> Int32.TryParse with | (true, y) -> (x = y)
+                                                                                                   | _ -> false
+                                               | _ -> false)
+
+                    | _ -> 0 = String.Compare(case.Name, value, true))
+                |> List.map(fun x -> FSharpValue.MakeUnion(x, [||]))
+                |> function | [] -> value |> sprintf "No union case value matched %s." |> failwith
+                            | x :: _ -> x
+
+        let all =
+            [|
+                new OptionConverter() :> JsonConverter
+                new TupleConverter() :> JsonConverter
+                new UnionConverter() :> JsonConverter
+            |]
+
 
     let tryParse = Option.tryFunction JsonValue.Parse
     let tryGet (property:string) (json:JsonValue) = json.TryGetProperty(property)
@@ -17,11 +131,11 @@ module JsonValue =
 
     let toString (json:JsonValue) = json.ToString(JsonSaveOptions.DisableFormatting)
     let toBytes = toString >> fst Codec.StringToBytes
-    let toType (json:JsonValue) : 'T = JsonConvert.DeserializeObject<'T>(toString json)
+    let toType (json:JsonValue) : 'T = JsonConvert.DeserializeObject<'T>(toString json, Converters.all)
 
     let ofString (json:string) = JsonValue.Parse json
     let ofBytes = fst Codec.BytesToString >> ofString
-    let ofType (t:'T) : JsonValue = JsonConvert.SerializeObject(t) |> ofString
+    let ofType (t:'T) : JsonValue = JsonConvert.SerializeObject(t, Converters.all) |> ofString
 
     module Codec =
 
