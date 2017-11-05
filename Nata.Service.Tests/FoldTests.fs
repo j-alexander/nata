3460ab7e56f54065d57af002298ef3ce7ad9ef9d
Jonathan Leaver <jleaver@alumni.uwo.ca>
2017-11-05 17:22:37 -0500

    add a project for testing Nata.Service

---
+++ Nata.Service.Tests/FoldTests.fs
@@ -0,0 +1,15 @@
+﻿namespace Nata.Service.Tests
+
+open System
+open System.Text
+open FSharp.Data
+open NUnit.Framework
+open Nata.IO
+open Nata.IO.Capability
+
+[<TestFixture>]
+type FoldTests() =
+
+    [<Test>]
+    member x.TestStub() =
+        Assert.True(true)
