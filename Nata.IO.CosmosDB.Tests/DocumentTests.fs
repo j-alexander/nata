e7156c78b87528680673051695c716916c8cd211
Jonathan Leaver <jonathan@jet.com>
2017-11-20 13:06:13 -0500

    add projects for Nata.IO.CosmosDB

---
+++ Nata.IO.CosmosDB.Tests/DocumentTests.fs
@@ -0,0 +1,16 @@
+﻿namespace Nata.IO.CosmosDB.Tests
+
+open NUnit.Framework
+open FSharp.Data
+open Nata.Core
+open Nata.IO
+open Nata.IO.CosmosDB
+
+[<TestFixture>]
+type DocumentTests() = 
+
+    [<Test>]
+    member x.TestConnect() =
+        let channel : Channel<JsonValue, string> = Document.connect <| guid()
+        let expected : Channel<JsonValue, string> = []
+        Assert.AreEqual(expected, channel)
---
+++ Nata.IO.CosmosDB/Document.fs
@@ -0,0 +1,10 @@
+﻿namespace Nata.IO.CosmosDB
+
+open Nata.IO
+
+module Document =
+
+    let connect : Source<'a,'b,'c> =
+        fun _ ->
+            [
+            ]
