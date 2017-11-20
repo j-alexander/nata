66eae0f1cd7812d8ba2d76b90a8c07132041e3a7
Jonathan Leaver <jonathan@jet.com>
2017-11-20 14:31:43 -0500

    add nuget Microsoft.Azure.DocumentDB

--- Nata.IO.CosmosDB/Document.fs
+++ Nata.IO.CosmosDB/Document.fs
@@ -4,6 +4,10 @@ open Nata.IO
 
 module Document =
 
+    let emulator =
+        "https://localhost:8081",
+        "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+
     let connect : Source<'a,'b,'c> =
         fun _ ->
             [
