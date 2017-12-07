70485d06c360a6fe641c77732c084027c4dbb17c
Jonathan Leaver <jonathan@jet.com>
2017-12-07 12:48:54 -0500

    add a distribution hub for snapshot data

---
+++ Nata.Service.Tests/HubTests.fs
@@ -0,0 +1,70 @@
+﻿namespace Nata.Service.Tests
+
+open System
+open System.Collections.Concurrent
+open System.Text
+open FSharp.Data
+open NUnit.Framework
+open Nata.Core
+open Nata.IO
+open Nata.IO.Capability
+open Nata.IO.Memory
+open Nata.Service
+
+[<TestFixture>]
+type HubTests() =
+
+    let connect() =
+        let channel =
+            Stream.connect()
+            <| guid()
+        channel
+        |> Channel.writer,
+        channel
+        |> Channel.subscriberFrom
+        |> Hub.Snapshot.create
+
+    [<Test>]
+    member x.TestLastSnapshot() =
+        let write, hub = connect()
+
+        let complete =
+            Async.StartAsTask <| async {
+                hub()
+                |> Seq.map Event.data
+                |> Seq.takeWhile ((<>) 10)
+                |> Seq.iter ignore
+            }
+
+        seq { 1..10 }
+        |> Seq.map Event.create
+        |> Seq.iter write
+
+        complete.Wait()
+
+        for _ in 1..1000 do
+            Assert.AreEqual(
+                10,
+                hub()
+                |> Seq.map Event.data
+                |> Seq.head)
+
+    [<Test>]
+    member x.TestFullSnapshotSeq() =
+        let write, hub = connect()
+
+        let queue = new BlockingCollection<int>()
+        let complete =
+            Async.StartAsTask <| async {
+                hub()
+                |> Seq.map Event.data
+                |> Seq.take 1000
+                |> Seq.iter queue.Add
+            }
+
+        seq { 1..1000 }
+        |> Seq.log (Event.create >> write)
+        |> Seq.iter(fun i ->
+            Assert.AreEqual(i, queue.Take()))
+
+        complete.Wait()
---
+++ Nata.Service/Hub.fs
@@ -0,0 +1,71 @@
+﻿namespace Nata.Service
+
+open System
+open Nata.Core
+open Nata.IO
+
+module Hub =
+
+    module Snapshot =
+
+        type private Listener<'Data,'Index> =
+            AsyncReplyChannel<Event<'Data>*'Index>
+
+        type private Listeners<'Data,'Index> =
+            Listener<'Data,'Index> list
+
+        type private Message<'Data,'Index> =
+            | Publish of Event<'Data>*'Index
+            | Subscribe of Listener<'Data,'Index>
+            | Next of Listener<'Data,'Index>*'Index
+
+        let create (subscribeFrom : SubscriberFrom<'Data,'Index>) =
+
+            let hub = MailboxProcessor<Message<'Data,'Index>>.Start <| fun inbox ->
+
+                Async.Start <| async {
+                    subscribeFrom(Position.Before Position.End)
+                    |> Seq.iter(Publish >> inbox.Post)
+                }
+
+                let rec loop(snapshot, index, listeners:Listeners<'Data,'Index>) =
+                    async {
+                        let! message = inbox.Receive()
+                        match message with
+                        | Publish (snapshot, index) ->
+                            for listener in listeners do
+                                listener.Reply(snapshot, index)
+                            return! loop(snapshot, index, [])
+                        | Next (listener, last) when last=index ->
+                            return! loop(snapshot, index, listener :: listeners)
+                        | Next (listener, _)
+                        | Subscribe (listener) ->
+                            do listener.Reply(snapshot, index)
+                            return! loop(snapshot, index, listeners)
+                    }
+
+                let rec start(listeners:Listeners<'Data,'Index>) =
+                    async {
+                        let! message = inbox.Receive()
+                        match message with
+                        | Publish (snapshot, index) ->
+                            for listener in listeners do
+                                listener.Reply(snapshot, index)
+                            return! loop(snapshot, index, [])
+                        | Next (listener, _)
+                        | Subscribe (listener) ->
+                            return! start(listener :: listeners)
+                    }
+
+                start[]
+
+            fun _ ->
+                let rec loop(snapshot, index) =
+                    seq {
+                        yield snapshot
+                        yield!
+                            hub.PostAndReply(fun reply -> Next(reply, index))
+                            |> loop
+                    }
+                hub.PostAndReply(Subscribe)
+                |> loop
