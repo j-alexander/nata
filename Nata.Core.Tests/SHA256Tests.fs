664db1de463abcfb5e1520d7a50ca0c123f2929c
Jonathan Leaver <jleaver@alumni.uwo.ca>
2018-03-31 22:06:20 -0400

    add unit tests for the SHA256 algorithm

---
+++ Nata.Core.Tests/SHA256Tests.fs
@@ -0,0 +1,80 @@
+﻿namespace Nata.Core.Tests
+
+open System
+open System.Text
+open NUnit.Framework
+open Nata.Core
+
+[<TestFixture>]
+type SHA256Tests() =
+
+    [<Test>]
+    member x.TestEmptyInput() =
+        Assert.AreEqual(
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+            SHA256.hash [||])
+        Assert.AreEqual(
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+            SHA256.hash (Encoding.ASCII.GetBytes ""))
+        Assert.AreEqual(
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+            SHA256.hash (Encoding.UTF8.GetBytes ""))
+
+    [<Test>]
+    member x.TestCommonExamples() =
+        Assert.AreEqual(
+            "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592",
+            SHA256.hashUTF8 "The quick brown fox jumps over the lazy dog")
+        Assert.AreEqual(
+            "ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c",
+            SHA256.hashUTF8 "The quick brown fox jumps over the lazy dog.")
+        Assert.AreEqual(
+            "72726d8818f693066ceb69afa364218b692e62ea92b385782363780f47529c21",
+            SHA256.hashUTF8 "中文")
+        Assert.AreEqual(
+            "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad",
+            SHA256.hashUTF8 "abc")
+
+    [<Test>]
+    member x.TestGeneratedExamples() =
+        [ 1..1000 ]
+        |> List.map guidBytes
+        |> List.iter(fun input ->
+            Assert.AreEqual(
+                SHA256.reference input,
+                SHA256.hash input))
+
+    [<Test>]
+    member x.Test32kExample() =
+        let input =
+            [| 1..2048 |]
+            |> Array.collect guidBytes
+        Assert.AreEqual(
+            SHA256.reference input,
+            SHA256.hash input)
+
+    [<Test>]
+    member x.TestCompress() =
+        let a = 0x87564c0cu
+        let b = 0xf1369725u
+        let c = 0x82e6d493u
+        let d = 0x63a6b509u
+        let e = 0xdd9eff54u
+        let f = 0xe07c2655u
+        let g = 0xa41f32e7u
+        let h = 0xc7d25631u
+
+        let k = 0xc67178f2u
+        let w = 0x6534ea14u
+
+        let h0,h1,h2,h3,h4,h5,h6,h7 =
+            SHA256.compress (a,b,c,d,e,f,g,h) (k,w)
+
+        Assert.AreEqual(0xe620b22bu, h0)
+        Assert.AreEqual(0x87564c0cu, h1)
+        Assert.AreEqual(0xf1369725u, h2)
+        Assert.AreEqual(0x82e6d493u, h3)
+        Assert.AreEqual(0xadcef783u, h4)
+        Assert.AreEqual(0xdd9eff54u, h5)
+        Assert.AreEqual(0xe07c2655u, h6)
+        Assert.AreEqual(0xa41f32e7u, h7)
--- Nata.Core/SHA256.fs
+++ Nata.Core/SHA256.fs
@@ -1,127 +1,119 @@
-﻿module SHA256
-
-let h =
-      0x6a09e667u, 0xbb67ae85u, 0x3c6ef372u, 0xa54ff53au, 0x510e527fu, 0x9b05688cu, 0x1f83d9abu, 0x5be0cd19u
-
-let k =
-    [ 0x428a2f98u; 0x71374491u; 0xb5c0fbcfu; 0xe9b5dba5u; 0x3956c25bu; 0x59f111f1u; 0x923f82a4u; 0xab1c5ed5u;
-      0xd807aa98u; 0x12835b01u; 0x243185beu; 0x550c7dc3u; 0x72be5d74u; 0x80deb1feu; 0x9bdc06a7u; 0xc19bf174u;
-      0xe49b69c1u; 0xefbe4786u; 0x0fc19dc6u; 0x240ca1ccu; 0x2de92c6fu; 0x4a7484aau; 0x5cb0a9dcu; 0x76f988dau;
-      0x983e5152u; 0xa831c66du; 0xb00327c8u; 0xbf597fc7u; 0xc6e00bf3u; 0xd5a79147u; 0x06ca6351u; 0x14292967u;
-      0x27b70a85u; 0x2e1b2138u; 0x4d2c6dfcu; 0x53380d13u; 0x650a7354u; 0x766a0abbu; 0x81c2c92eu; 0x92722c85u;
-      0xa2bfe8a1u; 0xa81a664bu; 0xc24b8b70u; 0xc76c51a3u; 0xd192e819u; 0xd6990624u; 0xf40e3585u; 0x106aa070u;
-      0x19a4c116u; 0x1e376c08u; 0x2748774cu; 0x34b0bcb5u; 0x391c0cb3u; 0x4ed8aa4au; 0x5b9cca4fu; 0x682e6ff3u;
-      0x748f82eeu; 0x78a5636fu; 0x84c87814u; 0x8cc70208u; 0x90befffau; 0xa4506cebu; 0xbef9a3f7u; 0xc67178f2u ]
-
-let rotr(x:uint32, n) =
-    assert(n < 32)
-    (x >>> n) ||| (x <<< (32 - n))
-
-let ch(x:uint32, y:uint32, z:uint32) =
-    (x &&& y) ^^^ ((~~~x) &&& z)
-
-let maj(x:uint32, y:uint32, z:uint32) =
-    (x &&& y) ^^^ (x &&& z) ^^^ (y &&& z)
-
-let Σ0 x = rotr(x,2) ^^^ rotr(x,13) ^^^ rotr(x,22)
-let Σ1 x = rotr(x,6) ^^^ rotr(x,11) ^^^ rotr(x,25)
-
-let σ0 x = rotr(x,7) ^^^ rotr(x,18) ^^^ (x >>> 3)
-let σ1 x = rotr(x,17) ^^^ rotr(x,19) ^^^ (x >>> 10)
-
-let compress (a,b,c,d,e,f,g,h) (k,w) =
-    let t1 = h + Σ1(e) + ch(e,f,g) + k + w
-    let t2 = Σ0(a) + maj(a,b,c)
-    (t1+t2,a,b,c,d+t1,e,f,g)
-
-let processChunk (h0,h1,h2,h3,h4,h5,h6,h7) (chunk:uint32[]) =
-    assert(chunk.Length = 16)
-
-    let w = Array.init 64 (fun _ -> 0u)
-    for i in 0..15 do
-        w.[i] <- chunk.[i]
-    for i in 16..63 do
-        w.[i] <- σ1(w.[i-2]) + w.[i-7] + σ0(w.[i-15]) + w.[i-16]
-
-    let (a,b,c,d,e,f,g,h) =
-        [0..63]
-        |> List.map (fun i -> k.[i], w.[i])
-        |> List.fold compress (h0,h1,h2,h3,h4,h5,h6,h7)
-
-    (h0+a, h1+b, h2+c, h3+d, h4+e, h5+f, h6+g, h7+h)
+﻿namespace Nata.Core
 
 open System
+open System.Text
+open System.Security.Cryptography
 
-let hash (data:byte[]) =
+module SHA256 =
 
-    let data_bits = 8 * data.Length
-    let pad_bits = 512 - ((data_bits + 1 + 64) % 512)
+    let h = 0x6a09e667u, 0xbb67ae85u, 0x3c6ef372u, 0xa54ff53au, 0x510e527fu, 0x9b05688cu, 0x1f83d9abu, 0x5be0cd19u
 
-    let padding =
-        Array.init ((1+pad_bits)/8) (function 0 -> 0x80uy | _ -> 0uy)
-    let length =
-        BitConverter.GetBytes(int64 data_bits)
+    let k =
+        [ 0x428a2f98u; 0x71374491u; 0xb5c0fbcfu; 0xe9b5dba5u; 0x3956c25bu; 0x59f111f1u; 0x923f82a4u; 0xab1c5ed5u;
+          0xd807aa98u; 0x12835b01u; 0x243185beu; 0x550c7dc3u; 0x72be5d74u; 0x80deb1feu; 0x9bdc06a7u; 0xc19bf174u;
+          0xe49b69c1u; 0xefbe4786u; 0x0fc19dc6u; 0x240ca1ccu; 0x2de92c6fu; 0x4a7484aau; 0x5cb0a9dcu; 0x76f988dau;
+          0x983e5152u; 0xa831c66du; 0xb00327c8u; 0xbf597fc7u; 0xc6e00bf3u; 0xd5a79147u; 0x06ca6351u; 0x14292967u;
+          0x27b70a85u; 0x2e1b2138u; 0x4d2c6dfcu; 0x53380d13u; 0x650a7354u; 0x766a0abbu; 0x81c2c92eu; 0x92722c85u;
+          0xa2bfe8a1u; 0xa81a664bu; 0xc24b8b70u; 0xc76c51a3u; 0xd192e819u; 0xd6990624u; 0xf40e3585u; 0x106aa070u;
+          0x19a4c116u; 0x1e376c08u; 0x2748774cu; 0x34b0bcb5u; 0x391c0cb3u; 0x4ed8aa4au; 0x5b9cca4fu; 0x682e6ff3u;
+          0x748f82eeu; 0x78a5636fu; 0x84c87814u; 0x8cc70208u; 0x90befffau; 0xa4506cebu; 0xbef9a3f7u; 0xc67178f2u ]
 
-    let batch n =
-        Seq.windowed n
-        >> Seq.mapi (fun i x -> i,x)
-        >> Seq.choose (fun (i,x) -> if i % n = 0 then Some x else None)
+    let rotr(x:uint32, n) =
+        assert(n < 32)
+        (x >>> n) ||| (x <<< (32 - n))
 
-    let chunks =
-        seq {
-            yield! data
-            yield! padding
-            yield! length
-        }
-        |> batch 4
-        |> Seq.map (fun x -> BitConverter.ToUInt32(Array.rev x,0))
-        |> batch 16
+    let ch(x:uint32, y:uint32, z:uint32) =
+        (x &&& y) ^^^ ((~~~x) &&& z)
 
-    let (h0,h1,h2,h3,h4,h5,h6,h7) =
-        chunks
-        |> Seq.fold processChunk h
+    let maj(x:uint32, y:uint32, z:uint32) =
+        (x &&& y) ^^^ (x &&& z) ^^^ (y &&& z)
 
-    [| h0; h1; h2; h3; h4; h5; h6; h7 |]
-    |> Seq.collect (BitConverter.GetBytes >> Array.rev)
-    |> Seq.map (sprintf "%02x")
-    |> String.Concat
+    let Σ0 x = rotr(x,2) ^^^ rotr(x,13) ^^^ rotr(x,22)
+    let Σ1 x = rotr(x,6) ^^^ rotr(x,11) ^^^ rotr(x,25)
 
-let reference (data:byte[]) =
-    (new System.Security.Cryptography.SHA256Managed()).ComputeHash(data)
-    |> Seq.map (sprintf "%02x")
-    |> String.Concat
+    let σ0 x = rotr(x,7) ^^^ rotr(x,18) ^^^ (x >>> 3)
+    let σ1 x = rotr(x,17) ^^^ rotr(x,19) ^^^ (x >>> 10)
 
-let compare (data:byte[]) =
-    sprintf "expect %s, and got %s" (reference data) (hash data)
+    let compress (a,b,c,d,e,f,g,h) (k,w) =
+        let t1 = h + Σ1(e) + ch(e,f,g) + k + w
+        let t2 = Σ0(a) + maj(a,b,c)
+        (t1+t2,a,b,c,d+t1,e,f,g)
 
-let testCompress() =
-    let a = 0x87564c0cu
-    let b = 0xf1369725u
-    let c = 0x82e6d493u
-    let d = 0x63a6b509u
-    let e = 0xdd9eff54u
-    let f = 0xe07c2655u
-    let g = 0xa41f32e7u
-    let h = 0xc7d25631u
+    let processChunk (h0,h1,h2,h3,h4,h5,h6,h7) (chunk:uint32[]) =
+        assert(chunk.Length = 16)
 
-    let k = 0xc67178f2u
-    let w = 0x6534ea14u
+        let w = Array.init 64 (fun _ -> 0u)
+        for i in 0..15 do
+            w.[i] <- chunk.[i]
+        for i in 16..63 do
+            w.[i] <- σ1(w.[i-2]) + w.[i-7] + σ0(w.[i-15]) + w.[i-16]
 
-    let h0,h1,h2,h3,h4,h5,h6,h7 =
-        compress (a,b,c,d,e,f,g,h) (k,w)
+        let (a,b,c,d,e,f,g,h) =
+            [0..63]
+            |> List.map (fun i -> k.[i], w.[i])
+            |> List.fold compress (h0,h1,h2,h3,h4,h5,h6,h7)
 
-    assert(0xe620b22bu = h0)
-    assert(0x87564c0cu = h1)
-    assert(0xf1369725u = h2)
-    assert(0x82e6d493u = h3)
-    assert(0xadcef783u = h4)
-    assert(0xdd9eff54u = h5)
-    assert(0xe07c2655u = h6)
-    assert(0xa41f32e7u = h7)
+        (h0+a, h1+b, h2+c, h3+d, h4+e, h5+f, h6+g, h7+h)
 
-let toHexes (h0,h1,h2,h3,h4,h5,h6,h7) =
-    let h:uint32[] = [| h0; h1; h2; h3; h4; h5; h6; h7 |]
-    h
-    |> Seq.map BitConverter.GetBytes
-    |> Seq.map (Seq.rev >> Seq.map (sprintf "%02x") >> String.Concat)
-    |> Seq.toArray
+    let hashBytes (data:byte[]) =
+
+        let data_bits = 8 * data.Length
+        let pad_bits = 512 - ((data_bits + 1 + 64) % 512)
+
+        let padding =
+            Array.init ((1+pad_bits)/8) (function 0 -> 0x80uy | _ -> 0uy)
+        let length =
+            BitConverter.GetBytes(int64 data_bits)
+            |> Array.rev
+
+        let batch n =
+            Seq.windowed n
+            >> Seq.mapi (fun i x -> i,x)
+            >> Seq.choose (fun (i,x) -> if i % n = 0 then Some x else None)
+
+        let chunks =
+            seq {
+                yield! data
+                yield! padding
+                yield! length
+            }
+            |> batch 4
+            |> Seq.map (fun x -> BitConverter.ToUInt32(Array.rev x,0))
+            |> batch 16
+
+        let (h0,h1,h2,h3,h4,h5,h6,h7) =
+            chunks
+            |> Seq.fold processChunk h
+
+        [| h0; h1; h2; h3; h4; h5; h6; h7 |]
+        |> Seq.collect (BitConverter.GetBytes >> Array.rev)
+        |> Seq.toArray
+
+    let hash =
+        hashBytes
+        >> Array.map (sprintf "%02x")
+        >> String.Concat
+
+    let hashUTF8Bytes (text:string) =
+        Encoding.UTF8.GetBytes(text)
+        |> hashBytes
+
+    let hashUTF8 (text:string) =
+        Encoding.UTF8.GetBytes(text)
+        |> hash
+
+    let referenceBytes (data:byte[]) =
+        (new SHA256Managed()).ComputeHash(data)
+
+    let reference =
+        referenceBytes
+        >> Seq.map (sprintf "%02x")
+        >> String.Concat
+
+    let referenceUTF8Bytes (text:string) =
+        Encoding.UTF8.GetBytes(text)
+        |> referenceBytes
+
+    let referenceUTF8 (text:string) =
+        Encoding.UTF8.GetBytes(text)
+        |> reference
