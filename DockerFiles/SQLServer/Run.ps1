a54b0ff93819e368ae23afbb5ff6686c1bdbe002
Jonathan Leaver <jleaver@alumni.uwo.ca>
2019-03-13 19:53:17 -0400

    partitions to 1; separate images for SQL server

--- DockerFiles/SQLServer/Run.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "Use a connection string as follows, after setting {ip}:"
-Write-Host "Server={ip};User Id=sa;Password=docker_13.0.4001.0"
-docker run --rm -p 1433:1433 -it nata/sqlserver
--- DockerFiles/Test/AzureStorage/Run-Service.ps1
+++
@@ -1,7 +0,0 @@
-if (-not (Test-Path "/AzureStorage/Configuration.info")) {
-     Write-Host "Running as:"
-     WhoAmI
-     AzureStorageEmulator.exe init -server localhost -forcecreate -inprocess
-     AzureStorageEmulator.exe status > /AzureStorage/Configuration.info
-}
-AzureStorageEmulator.exe start -inprocess
