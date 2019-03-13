220a93d2bea3b81b5381e062d8f5231caa29deb3
Jonathan Leaver <jleaver@alumni.uwo.ca>
2019-03-13 18:01:34 -0400

    improve test execution using mounted volumes

---
+++ Test-Local.ps1
@@ -0,0 +1,4 @@
+ForEach ($Test in $(ls *.Tests)) {
+  Write-Host "Testing $Test"
+  dotnet test -c Debug --no-build --no-restore $Test
+}
--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,19 @@
+$Source = $PSScriptRoot
+
+
 .paket/paket.bootstrapper.exe
 .paket/paket.exe restore
 dotnet restore
-dotnet build --no-restore
-docker build .\
+dotnet build -c Debug --no-restore 
+ForEach ($Test in $(ls *.Tests)) {
+dotnet publish -c Debug --no-restore --no-build --output bin\Debug\netcoreapp2.1 $Test
+}
+
+$Target = "C:\Nata"
+$Volume = $Source+":"+$Target
+Write-Host "Mounting $Source into the container as C:\Nata"
+
+$Command = "net start CosmosDB ; Set-Location /Nata ; ./Test-Local.ps1"
+
+docker run --rm -it -v $Volume nata/test PowerShell.exe -NoProfile -Command "$Command"
+
