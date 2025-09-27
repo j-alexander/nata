d6e3b0e961164bcd7694a821d1217bd4ed9200df
Jonathan Leaver <jleaver@alumni.uwo.ca>
2025-09-27 14:31:11 -0400

    PoC code using OpenCvSharp - not tested yet

--- Nata.IO.HLS.Tests/HLSTests.fs
+++ Nata.IO.HLS.Tests/HLSTests.fs
@@ -7,6 +7,11 @@ open System.Reflection
 open System.Threading
 open System.Threading.Tasks
 open FSharp.Data
+
+open Nata.IO.Event
+
+open OpenCvSharp
+//open OpenCvSharp.
 open NUnit.Framework
 
 open Nata.Core
@@ -17,30 +22,32 @@ open Nata.IO.HLS
 [<TestFixture>]
 type HLSTests() =
 
-    let event() =
-        Event.create(guid(), Guid.NewGuid().ToByteArray())
+    let testAddress =
+        "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8"
 
-    let connect = 
-        { Settings.Address="http://127.0.0.1:8500/" }
+    let connect() = 
+        { Settings.Address=testAddress }
         |> HLS.Client.connect
 
+    let profileMat(f: Mat) = 
+        printfn "--- Frame Profile Information ---"
+        printfn $"Dimensions (Width x Height): {f.Width} x {f.Height} pixels"
+        printfn $"Total Pixels: {f.Total()} pixels"
+        printfn $"Channels: {f.Channels()} (Color Depth)"
+        printfn $"Type (Internal OpenCV Code): {f.Type()}"
+        printfn $"Depth (Internal OpenCV Code): {f.Depth()}"
+        //printfn $"Total Data Size: {f.DataSize} bytes"
+        printfn $"Is Continuous: {f.IsContinuous} (Indicates data stored in a single contiguous block)"
+        printfn "---------------------------------"
+    
     [<Test>]
     member x.TestRead() =
         let read =
             connect()
             |> reader
 
-        for i in 1..3 do
-            //
-            // let expectKey, expectValue =
-            //     (), ()
-            //
-            // let result = 
-            //     read() 
-            //     |> Seq.filter (Event.data >> (fun _ -> true))
-            //     |> Seq.head
-            // let resultKey, resultValue = Event.data result
-            //
-            // Assert.AreEqual(expectKey, resultKey)
-            // Assert.AreEqual(expectValue, resultValue)
-            Assert.True(true)
+        read()
+        |> Seq.take 100
+        |> Seq.iter (fun { Event.Data = data } ->
+            
+            profileMat data)
--- Nata.IO.HLS/HLS.fs
+++ Nata.IO.HLS/HLS.fs
@@ -2,25 +2,49 @@
 
 open System
 open System.Text
+open OpenCvSharp
 open Nata.IO
 
-type Client = unit
+type Client = VideoCapture
 
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Client =
 
     let create { Settings.Address=address } =
-        ()
+        new VideoCapture(address, VideoCaptureAPIs.ANY)
     
-        
-    let read (client:Client) prefix =
-        Seq.empty
+    let read (settings:Settings) =
+        seq {
+            use client = create settings 
+            if client.IsOpened() then
+                printfn "Stream opened successfully. Reading frames..."
+                        
+                let rec readNextFrame () =
+                    seq {
+                        use mutable frame = new Mat()
+
+                        if client.Read(frame) then
+                            if frame.Empty() then
+                                printfn "End of stream"
+                                () //end
+                            else
+                                yield
+                                    frame.Clone()
+                                    |> Event.create
+                                yield! readNextFrame ()
+                        else
+                            printfn "Read failed"
+                            ()
+                    }
+                yield! readNextFrame()
+            else
+                printfn "!!! Error: Could not open video stream"
+                yield! Seq.empty 
+        }
 
     let connect (settings:Settings) =
-        let client = create settings
-        fun prefix ->
-            [
-                Capability.Reader <| fun () ->
-                    read client prefix
-            ]
+        [
+            Capability.Reader <| fun () ->
+                read settings
+        ]
